033134bf106f5c3a4e007513678740539ec8962d
urvashiramdasani <urvashi.ramdasani@gmail.com>
2021-09-06 22:05:46 +0530

    some changes

--- detectApp/views.py
+++ detectApp/views.py
@@ -34,77 +34,76 @@ class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 class UPLOADFILE(APIView):
-    pass
 
-def upload_file(request):
-    results = ""
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            newdoc = Document(docfile = request.FILES['docfile'])
-            newdoc.save()
-            filename = newdoc.docfile.name
-            results = detect_forgery(filename = filename)
-            # results['img_path_result'] = img_path_result
-            # results['label'] = 'Forged' if is_pair_found else 'Not Forged'
-    else:
-        form = DocumentForm()
-        return render(request, 'detectApp/upload_file.html', {'form': form})
+    def upload_file(self, request):
+        results = ""
+        if request.method == 'POST':
+            form = DocumentForm(request.POST, request.FILES)
+            if form.is_valid():
+                newdoc = Document(docfile = request.FILES['docfile'])
+                newdoc.save()
+                filename = newdoc.docfile.name
+                results = self.detect_forgery(filename = filename)
+                # results['img_path_result'] = img_path_result
+                # results['label'] = 'Forged' if is_pair_found else 'Not Forged'
+        else:
+            form = DocumentForm()
+            return render(request, 'detectApp/upload_file.html', {'form': form})
 
-    return render(request, 'detectApp/upload_file.html', {'results': results})
+        return render(request, 'detectApp/upload_file.html', {'results': results})
 
-# Create your views here.
-def process_image(img_path, parameters):
-    # print('Processing image: ',img_path)
-    score = np.nan
-    try:
-        X, RGB, NP, RGN,im_mode = loader(img_path, parameters.mode)
-    except Exception as e:
-        print("Error in opening image file: ",img_path)
+    # Create your views here.
+    def process_image(img_path, parameters):
+        # print('Processing image: ',img_path)
+        score = np.nan
+        try:
+            X, RGB, NP, RGN,im_mode = loader(img_path, parameters.mode)
+        except Exception as e:
+            print("Error in opening image file: ",img_path)
+            return score
+
+        if np.min(X.shape[0:2])< (parameters.tile_size+parameters.tile_stride):
+            print('Image is too small:'+ img_path)
+        else:
+            try:
+                score = detection(X, RGB, NP, RGN, parameters.mode)
+                print('> Score: {}'.format(score))
+                # Update Output
+            except Exception as e:
+                print("Error in processing the image: ",img_path)
+                raise e
+
         return score
 
-    if np.min(X.shape[0:2])< (parameters.tile_size+parameters.tile_stride):
-        print('Image is too small:'+ img_path)
-    else:
-        try:
-            score = detection(X, RGB, NP, RGN, parameters.mode)
-            print('> Score: {}'.format(score))
-            # Update Output
-        except Exception as e:
-            print("Error in processing the image: ",img_path)
-            raise e
-
-    return score
-
-def detect_forgery(filename):
-    import argparse
-    import glob
-    parser = argparse.ArgumentParser()
-    #parser.add_argument('-g', '--gpu'   , type=str, default=None)
-    parser.add_argument('-m','--mode', type=str, default='FUSION') #RGB | N | RGN | FUSION
-    parser.add_argument('-tile_size','--tile_size', type=int, default=256)
-    parser.add_argument('-tile_stride','--tile_stride', type=int, default=192)
-    parser.add_argument('-train_dataset','--train_dataset', type=str, default='E2E')
+    def detect_forgery(self, filename):
+        import argparse
+        import glob
+        parser = argparse.ArgumentParser()
+        #parser.add_argument('-g', '--gpu'   , type=str, default=None)
+        parser.add_argument('-m','--mode', type=str, default='FUSION') #RGB | N | RGN | FUSION
+        parser.add_argument('-tile_size','--tile_size', type=int, default=256)
+        parser.add_argument('-tile_stride','--tile_stride', type=int, default=192)
+        parser.add_argument('-train_dataset','--train_dataset', type=str, default='E2E')
 
 
-    config, _ = parser.parse_known_args()
-    parameters.use_cuda = torch.cuda.is_available() # To run on CPU when GPUs are not available
-    parameters.mode = config.mode
-    parameters.tile_size = config.tile_size
-    parameters.tile_stride = config.tile_stride
-    parameters.ds = config.train_dataset
-    preload(parameters.mode)
+        config, _ = parser.parse_known_args()
+        parameters.use_cuda = torch.cuda.is_available() # To run on CPU when GPUs are not available
+        parameters.mode = config.mode
+        parameters.tile_size = config.tile_size
+        parameters.tile_stride = config.tile_stride
+        parameters.ds = config.train_dataset
+        preload(parameters.mode)
 
 
-    images_path = glob.glob('media/' + filename)
-    result = ""
+        images_path = glob.glob('media/' + filename)
+        result = ""
 
-    for img_p in images_path:
-        s = process_image(img_path=img_p,parameters=parameters)
-        print(f"Model prediction value: {s}")
-        if s > 0.5:
-            result = "Forged" 
-        else:
-            result = "Not Forged"
+        for img_p in images_path:
+            s = self.process_image(img_path=img_p,parameters=parameters)
+            print(f"Model prediction value: {s}")
+            if s > 0.5:
+                result = "Forged" 
+            else:
+                result = "Not Forged"
 
-    return result
+        return result
